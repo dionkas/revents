a1be90b029f0d63f06372192f8e54edd1090f1a9
Dionysios Kastellanis <dionysios.kastellanis@gmail.com>
2021-01-16 15:26:21 +0200

    [DK] End of Section 9

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,8 +1,9 @@
-import { createStore } from "redux";
-import { devToolsEnhancer } from "redux-devtools-extension";
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./rootReducer";
+import thunk from "redux-thunk";
 
 // create store with reducer
 export function configureStore() {
-  return createStore(rootReducer, devToolsEnhancer());
+  return createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 }
